8e2793ed20c8dd00f25c3ee42d4e5ee811818b2a
Jörg Kubaile <jk@zettwerk.com>
2011-04-03 07:26:41 +0000

    Back to development: 0.2

--- README.txt
+++ README.txt
@@ -26,7 +26,7 @@ Put a i18nduder part to your buildout.cfg and define it like this::
          zettwerk.i18nduder
 
 
-It is important to include your instance eggs, cause duder gets the path of the package by importing it. Also note, that zettwerk.i18nduder includes a dependency to i18ndude. 
+It is important to include your instance eggs, cause duder gets the path of the package by importing it. Also note, that zettwerk.i18nduder includes a dependency to i18ndude.
 
 
 Available commands and usage
@@ -41,12 +41,15 @@ The generated script is called duder. To see a list of commands use the --help a
   >   create		Create the locales folder and/or add given languages
   >   update		Call rebuild-pot and sync
   >   mo		Call msgfmt to build the mo files
-  >   find		Find untranslated strings
+  >   find		Find untranslated strings (domain independent)
   >
   > Options:
   >   -h, --help            show this help message and exit
   >   -p PACKAGE, --package=PACKAGE
   >                         Name of the package
+  >   -d DOMAIN, --domain=DOMAIN
+  >                         Name of the i18n domain. Only needed if it is different
+  >                         from the package name.
   >   -l LANGUAGES, --languages=LANGUAGES
   >                         List of comma-separated names of language strings.
   >                         Only used for create command. For example: -l en,de
@@ -90,5 +93,10 @@ And the mo files can be compiled like this::
   > ./bin/duder mo -p dummy.example
 
 This calls "msgfmt", so it must be available on your system.
+
+Notes about domains
+===================
+
+It is maybe good to now, that i18ndude can't differentiate domains by the MessageFactory. So if you run duder with the domain option will result with the msgids of the given domain (when used explicitly in your template files with i18n:domain) _and_ the translations, which are created via the MessageFactory of you default domain.
 
 Feel free to contact us for suggestions and improvments.
--- zettwerk/i18nduder/duder.py
+++ zettwerk/i18nduder/duder.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-import i18ndude.script
-import sys
 import os
 import os.path
 import optparse
-import subprocess
-from StringIO import StringIO
+
+from commands import create, update, mo, find
 
 
 class UnformatedDescriptionFormatter(optparse.IndentedHelpFormatter):
@@ -15,13 +13,24 @@ class UnformatedDescriptionFormatter(optparse.IndentedHelpFormatter):
         return description
 
 
+def handle_command(command, options, parser):
+    if command == 'create':
+        create(options, parser)
+    if command == 'update':
+        update(options, parser)
+    if command == 'find':
+        find(options, parser)
+    if command == 'mo':
+        mo(options, parser)
+
+
 def main():
     usage = 'usage: %prog command -p package.name or --help for details'
     description = u'''Available commands:
   create\t\tCreate the locales folder and/or add given languages
   update\t\tCall rebuild-pot and sync
   mo\t\t\tCall msgfmt to build the mo files
-  find\t\t\tFind untranslated strings
+  find\t\t\tFind untranslated strings (domain independent)
 '''
     parser = optparse.OptionParser(usage=usage,
                                    description=description,
@@ -29,8 +38,13 @@ def main():
 
     parser.add_option('-p', '--package',
                       help="Name of the package")
+    parser.add_option('-d', '--domain',
+                      help="Name of the i18n domain. Only needed if it is " \
+                          "different from the package name.")
     parser.add_option('-l', '--languages',
-                      help="List of comma-separated names of language strings. Only used for create command. For example: -l en,de")
+                      help="List of comma-separated names of language " \
+                          "strings. Only used for create command. For " \
+                          "example: -l en,de")
 
     options, args = parser.parse_args()
     if not args:
@@ -39,101 +53,8 @@ def main():
     if not options.package:
         parser.error('no package given')
 
-    package_name = options.package
-    try:
-        __import__(package_name)
-        package_path = sys.modules[package_name].__path__[0]
-    except:
-        parser.error('Invalid package name given')
-
-    locales_path = os.path.join(package_path,
-                                'locales')
     working_dir = os.getcwd()
 
     for command in args:
         os.chdir(working_dir)
-        if command == 'create':
-            if not os.path.exists(locales_path):
-                os.mkdir(locales_path)
-                print "Created locales folder at: %s" % (locales_path)
-                print "Don't forget to add this to your configure.zcml:"
-                print '<i18n:registerTranslations directory="locales" />'
-            else:
-                print "Locales folder already exists at: %s" % (locales_path)
-            if options.languages:
-                for lang in options.languages.split(','):
-                    lang = lang.strip()
-                    if lang:
-                        if not os.path.exists(os.path.join(locales_path, lang)):
-                            os.mkdir(os.path.join(locales_path, lang))
-                            os.mkdir(os.path.join(locales_path, lang, 'LC_MESSAGES'))
-                            empty = file(os.path.join(locales_path, lang, 'LC_MESSAGES', '%s.po' % (package_name)), 'w').close()
-                            print "- %s language added" % (lang)
-                        else:
-                            print "- %s language already exists" % (lang)
-            else:
-                print "No languages given - skipping creation"
-
-        if command == 'update':
-            if not os.path.exists(locales_path):
-                parser.error('locales directory not found - run create command first')
-            os.chdir(locales_path)
-
-            dude_command = 'rebuild-pot'
-            dude_opts = ['--pot',
-                         '%s.pot' % (package_name),
-
-                         '--create',
-                         package_name,
-                         os.path.join('.', '..')]
-            sys.argv[1:] = [dude_command] + dude_opts
-            i18ndude.script.main()
-
-            dude_command = 'sync'
-            for lang in os.listdir('.'):
-                if os.path.isdir(os.path.join('.', lang)) and \
-                        'LC_MESSAGES' in os.listdir(os.path.join('.', lang)):
-                    dude_opts = ['--pot',
-                                 '%s.pot' % (package_name),
-                                 '%s/LC_MESSAGES/%s.po' % (lang, package_name)]
-                    sys.argv[1:] = [dude_command] + dude_opts
-                    i18ndude.script.main()
-
-        if command == 'find':
-            os.chdir(package_path)
-
-            output_file = os.path.join(working_dir,
-                                       '%s-untranslated' % (package_name))
-            dude_command = 'find-untranslated'
-            dude_opts = ['.']
-            sys.argv[1:] = [dude_command] + dude_opts
-
-            output = StringIO()
-            sys.stdout = output
-
-            i18ndude.script.main()
-
-            sys.stdout = sys.__stdout__
-
-            f = file(output_file, 'w')
-            f.write(output.getvalue())
-            f.close()
-
-            print "wrote output to: %s" % (output_file)
-
-        if command == 'mo':
-            if not os.path.exists(locales_path):
-                parser.error('locales directory not found - run create command first')
-
-            os.chdir(locales_path)
-
-            for lang in os.listdir('.'):
-                if os.path.isdir(os.path.join('.', lang)) and \
-                        'LC_MESSAGES' in os.listdir(os.path.join('.', lang)):
-                    args = ['msgfmt',
-                            '%s/LC_MESSAGES/%s.po' % (lang,
-                                                      package_name),
-                            '-o',
-                            '%s/LC_MESSAGES/%s.mo' % (lang,
-                                                      package_name)]
-                    subprocess.call(args)
+        handle_command(command, options, parser)
