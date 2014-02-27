4997e58ed79657eb00171bcc47cbc25c865b6088
Jörg Kubaile <jk@zettwerk.com>
2014-02-27 13:11:41 +0100

    readme renamed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.2.1'
 setup(name='zettwerk.i18nduder',
       version=version,
       description="A helper/wrapper script for i18ndude to easier working with translation files in a common plone environment.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
